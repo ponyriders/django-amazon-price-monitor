dc6b25dc96e5ff615c4568bbd9f21053faaffe47
Martin Mrose <martin@penthranet.de>
2013-05-16 15:09:40 +0200

    Added setup.py and CHANGELOG

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='django-amazon-price-monitor',
+    description='share django users and groups with other services via ldap',
+    version='0.1.0',
+    author='Alexander Herrmann & Martin Mrose',
+    author_email='mrosemartin84@gmail.com',
+    license='MIT',
+    url='https://github.com/ponyriders/django-amazon-price-monitor',
+    packages=['django_amazon_price_monitor'],
+    long_description=open('README.md').read(),
+    install_requires=[
+        'python-amazon-simple-product-api==1.4.0',
+    ],
+    dependency_links=[
+    ]
+)
