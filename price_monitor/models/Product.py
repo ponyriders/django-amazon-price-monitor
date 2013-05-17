e83bb6bae28eaea6a0b49c7286979f523d9b841f
Alexander Herrmann <darignac@googlemail.com>
2013-05-17 12:16:01 +0200

    added more fields to Product

--- price_monitor/models/Product.py
+++ price_monitor/models/Product.py
@@ -1,14 +1,26 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils.translation import ugettext_lazy
+from django.utils.translation import ugettext as _, ugettext_lazy
 
 
 class Product(models.Model):
     """
     Product to be monitored
     """
-    users = models.ManyToManyField(User, verbose_name=ugettext_lazy('Users monitoring this product'))
-    prices = models.ManyToManyField('Price', verbose_name=ugettext_lazy('Prices of Product'))
+    STATUS_CHOICES = (
+        (0, _('Created'),),
+        (1, _('Synced over API'),),
+    )
+
+    date_creation = models.DateTimeField(auto_now_add=True, verbose_name=_('Date of creation'))
+    date_updated = models.DateTimeField(auto_now=True, verbose_name=_('Date of last update'))
+    users = models.ManyToManyField(User, verbose_name=_('Users monitoring this product'))
+    prices = models.ManyToManyField('Price', verbose_name=_('Prices of Product'))
+    title = models.CharField(max_length=255, verbose_name=_('Title'))
+    asin = models.CharField(max_length=100, verbose_name=_('ASIN'))
+    status = models.SmallIntegerField(choices=STATUS_CHOICES, verbose_name=_('Status'))
+    large_image_url = models.URLField(verbose_name=_('URL to large product image'))
+    offer_url = models.URLField(verbose_name=_('URL to the offer'))
 
     class Meta:
         app_label = 'price_monitor'
