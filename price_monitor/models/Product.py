fd3cc2f66da87d90db0250370f122217b4108381
Alexander Herrmann <darignac@googlemail.com>
2013-05-20 15:45:02 +0200

    made user and price relation optional

--- price_monitor/models/Product.py
+++ price_monitor/models/Product.py
@@ -14,8 +14,8 @@ class Product(models.Model):
 
     date_creation = models.DateTimeField(auto_now_add=True, verbose_name=_('Date of creation'))
     date_updated = models.DateTimeField(auto_now=True, verbose_name=_('Date of last update'))
-    users = models.ManyToManyField(User, verbose_name=_('Users monitoring this product'))
-    prices = models.ManyToManyField('Price', verbose_name=_('Prices of Product'))
+    users = models.ManyToManyField(User, blank=True, null=True, verbose_name=_('Users monitoring this product'))
+    prices = models.ManyToManyField('Price', blank=True, null=True, verbose_name=_('Prices of Product'))
     title = models.CharField(blank=True, null=True, max_length=255, verbose_name=_('Title'))
     asin = models.CharField(max_length=100, verbose_name=_('ASIN'))
     status = models.SmallIntegerField(choices=STATUS_CHOICES, default=0, verbose_name=_('Status'))
