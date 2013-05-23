f61ebd231ed9ed60a458fcdc27c22b044bb9e730
Alexander Herrmann <darignac@googlemail.com>
2013-05-23 20:44:04 +0200

    added admin action to reset products

--- price_monitor/admin.py
+++ price_monitor/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy
 
 from price_monitor.models import (
     Price,
@@ -12,6 +13,13 @@ class PriceAdmin(admin.ModelAdmin):
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('asin', 'title', 'status', 'date_updated', )
+
+    actions = ['reset_to_created',]
+
+    def reset_to_created(self, request, queryset):
+        queryset.update(status=0)
+    reset_to_created.short_description = ugettext_lazy('Reset to status "Created".')
+
 
 
 admin.site.register(Price, PriceAdmin)
