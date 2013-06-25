4700844a79a8f0a359d596f2a24387d083c98744
Martin Mrose <github@penthranet.de>
2013-06-25 22:08:54 +0200

    Modified form handling again

--- price_monitor/forms.py
+++ price_monitor/forms.py
@@ -1,12 +1,33 @@
 import re
 
 from . import app_settings as settings
+from .models.Product import Product
+from .models.Subscription import Subscription
 
 from django import forms
 from django.utils.translation import ugettext as _
 
 
-class CustomSubscriptionForm(forms.Form):
-    asin = forms.RegexField(label=_('ASIN'), regex=settings.PRICE_MONITOR_ASIN_REGEX)
-    email = forms.EmailField(label=_('E-Mail for notifications'))
-    price = forms.DecimalField(label=_('Price to notify'), max_digits=2)
+class SubscriptionCreationForm(forms.ModelForm):
+    product = forms.RegexField(label=_('ASIN'), regex=settings.PRICE_MONITOR_ASIN_REGEX)
+
+    class Meta:
+        fields = ('product', 'email_notification', 'price_limit')
+        model = Subscription
+
+    def clean(self):
+        cleaned_data = super(SubscriptionCreationForm, self).clean()
+        try:
+            product = Product.objects.get(asin=cleaned_data['product'])
+        except Product.DoesNotExist:
+            product = Product.objects.create(asin=cleaned_data['product'])
+        cleaned_data['product'] = product
+        return cleaned_data
+
+class SubscriptionUpdateForm(forms.ModelForm):
+    class Meta:
+        fields = ('product', 'email_notification', 'price_limit')
+        model = Subscription
+        widgets = {
+            'product': forms.TextInput(attrs={'readonly': True}),
+        }
