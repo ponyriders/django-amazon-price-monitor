b46dda258322ef522db291b4085cbc95d8eb49c4
Alexander Herrmann <darignac@googlemail.com>
2013-06-29 11:45:41 +0200

    setup test execution with coverage and flake8 for Travis CI

---
+++ price_monitor/test_settings.py
@@ -0,0 +1,25 @@
+import os
+
+
+TEST_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tests')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'price_monitor',
+]
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(TEST_DIR, 'static')
+
+TEST_RUNNER = 'discover_runner.DiscoverRunner'
+
+SECRET_KEY = 'F(fxm_9aKa9F_7e$!U1can%;%qc9A[.Jcx2lVCwWo3}*DL,y?H'
