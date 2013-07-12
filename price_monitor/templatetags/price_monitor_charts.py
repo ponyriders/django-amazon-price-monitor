e435b3e84cb8ad07cf0cce077ab8817a74884f02
Alexander Herrmann <darignac@googlemail.com>
2013-07-12 20:16:04 +0200

    some typo and documentation

--- price_monitor/templatetags/price_monitor_charts.py
+++ price_monitor/templatetags/price_monitor_charts.py
@@ -9,7 +9,11 @@ register = template.Library()
 
 @register.simple_tag
 def charts_load_js_amd_css(*args):
-    # TODO docu
+    """
+    Loads the javascript and stylesheets for the given chart types.
+    :param args: list of chart types.
+    :return: html string
+    """
     css_files = [
         'nv.d3.css',
     ]
@@ -37,12 +41,17 @@ def charts_load_js_amd_css(*args):
 
 
 @register.simple_tag
-def chart_line(width, height, data, show_yaxis, title, color):
-    # TODO css separieren
-    # TODO js auslagern sofern mgl.
-    # TODO bei tooltip datum anzeigen -> formatieren
-    # TODO minimize
-    # TODO docu
+def chart_line(width, height, data, show_y_axis, title, color):
+    """
+    Creates a fancy line chart using NVD3.
+    :param width: width of the chart as "500px"
+    :param height: height of the chart as "300px"
+    :param data: the data to be displayed. A list of dictionaries with x and y keys, e.g. [{x: 1, y:0}, {x:2, y:-1}]
+    :param show_y_axis: if to show the y axis
+    :param title: the title of the chart. Only used to separate if there are multiple charts.
+    :param color: the color to draw the chart with
+    :return: string with appropriate HTML
+    """
     return render_to_string(
         'price_monitor/templatetags/chart_line.html',
         {
@@ -50,7 +59,7 @@ def chart_line(width, height, data, show_yaxis, title, color):
             'data': data,
             'id': randint(0, 1000000000),
             'height': height,
-            'show_yaxis': True if show_yaxis == 1 else False,
+            'show_y_axis': True if show_y_axis == 1 else False,
             'title': title,
             'width': width,
         },
