090338a54fa1d530307690cbdea4f4bf55350e5f
Alexander Herrmann <darignac@googlemail.com>
2014-01-19 14:22:06 +0100

    some refactoring for #10

--- price_monitor/tasks.py
+++ price_monitor/tasks.py
@@ -25,7 +25,101 @@ from smtplib import SMTPServerDisconnected
 logger = logging.getLogger('price_monitor')
 
 
-class ProductSynchronizeTask(PeriodicTask):
+class ProductSynchronizationMixin(object):
+    """
+    Mixin encapsulating the functionality to update a product.
+    """
+
+    @staticmethod
+    def sync_product(amazon_product, product):
+        """
+        Synchronizes the given price_monitor.model.Product with the Amazon lookup product.
+        :param amazon_product: the Amazon product
+        :type amazon_product: amazon.api.AmazonProduct
+        :param product: the product to update
+        :type product: price_monitor.models.Product
+        """
+        now = timezone.now()
+
+        product.set_values_from_amazon_product(amazon_product)
+        product.status = 1
+        product.date_last_synced = now
+        product.save()
+
+        # tuple: (price, currency)
+        price = amazon_product.price_and_currency
+
+        if not price[0] is None:
+            # get all subscriptions of product that are subscribed to the current price or a higher one and
+            # whose owners have not been notified about that particular subscription price since before
+            # settings.PRICE_MONITOR_SUBSCRIPTION_RENOTIFICATION_MINUTES.
+            for sub in Subscription.objects.filter(
+                Q(
+                    product=product,
+                    price_limit__gte=price[0],
+                    date_last_notification__lte=(timezone.now() - timedelta(minutes=settings.PRICE_MONITOR_SUBSCRIPTION_RENOTIFICATION_MINUTES))
+                ) | Q(
+                    product=product,
+                    price_limit__gte=price[0],
+                    date_last_notification__isnull=True
+                )
+            ):
+                # TODO: how to handle failed notifications?
+                NotifySubscriberTask().delay(product, price[0], price[1], sub)
+
+            # create the price entry
+            Price.objects.create(
+                value=price[0],
+                currency=price[1],
+                date_seen=now,
+                product=product,
+            )
+
+    @staticmethod
+    def get_products_to_sync():
+        """
+        Returns the products to synchronize.
+        These are newly created products with status "0" or products that are older than settings.PRICE_MONITOR_AMAZON_PRODUCT_REFRESH_THRESHOLD_MINUTES.
+        :return: tuple with 0: dictionary with the Products and 1: if there are still products that need to be synced
+        :rtype: tuple
+        """
+        # prefer already synced products over newly created
+        products = {
+            p.asin: p for p in Product.objects.select_related().filter(
+                subscription__isnull=False,
+                date_last_synced__lte=(timezone.now() - timedelta(minutes=settings.PRICE_MONITOR_AMAZON_PRODUCT_REFRESH_THRESHOLD_MINUTES))
+            )
+        }
+
+        # there is still some space for products to sync, append newly created if available
+        if len(products) < settings.PRICE_MONITOR_AMAZON_PRODUCT_SYNCHRONIZE_COUNT:
+            return (
+                dict(
+                    products.items() + {
+                        p.asin: p for p in Product.objects.select_related().filter(status=0)
+                            .order_by('date_creation')[:(settings.PRICE_MONITOR_AMAZON_PRODUCT_SYNCHRONIZE_COUNT - len(products))]
+                    }.items()
+                ),
+                # set recall to true if there are more unsynched products than already included
+                Product.objects.select_related().filter(status=0).count() > settings.PRICE_MONITOR_AMAZON_PRODUCT_SYNCHRONIZE_COUNT - len(products)
+            )
+        else:
+            def take(n, iterable):
+                """
+                Takes n elements out of the given iterable.
+                :param n: number of elements to take
+                :type n: int
+                :param iterable; the iterable dict
+                :type iterable: dictionary-itemiterator
+                :returns: the resized dictionary
+                :rtype : dict
+                """
+                return dict(list(islice(iterable, n)))
+
+            return take(settings.PRICE_MONITOR_AMAZON_PRODUCT_SYNCHRONIZE_COUNT, products.iteritems()), True
+
+
+class ProductSynchronizeTask(PeriodicTask, ProductSynchronizationMixin):
     """
     Synchronizes Products in status "Created" (0) initially with Product API.
     """
@@ -75,92 +169,6 @@ class ProductSynchronizeTask(PeriodicTask):
             # finally, if there are more products that can be synchronized, recall the task
             if recall:
                 self.apply_async(countdown=120)
-
-    def get_products_to_sync(self):
-        """
-        Returns the products to synchronize.
-        These are newly created products with status "0" or products that are older than settings.PRICE_MONITOR_AMAZON_PRODUCT_REFRESH_THRESHOLD_MINUTES.
-        :return: tuple with 0: dictionary with the Products and 1: if there are still products that need to be synced
-        :rtype: tuple
-        """
-        # prefer already synced products over newly created
-        products = {
-            p.asin: p for p in Product.objects.select_related().filter(
-                subscription__isnull=False,
-                date_last_synced__lte=(timezone.now() - timedelta(minutes=settings.PRICE_MONITOR_AMAZON_PRODUCT_REFRESH_THRESHOLD_MINUTES))
-            )
-        }
-
-        # there is still some space for products to sync, append newly created if available
-        if len(products) < settings.PRICE_MONITOR_AMAZON_PRODUCT_SYNCHRONIZE_COUNT:
-            return (
-                dict(
-                    products.items() + {
-                        p.asin: p for p in Product.objects.select_related().filter(status=0)
-                            .order_by('date_creation')[:(settings.PRICE_MONITOR_AMAZON_PRODUCT_SYNCHRONIZE_COUNT - len(products))]
-                    }.items()
-                ),
-                # set recall to true if there are more unsynched products than already included
-                Product.objects.select_related().filter(status=0).count() > settings.PRICE_MONITOR_AMAZON_PRODUCT_SYNCHRONIZE_COUNT - len(products)
-            )
-        else:
-            def take(n, iterable):
-                """
-                Takes n elements out of the given iterable.
-                :param n: number of elements to take
-                :type n: int
-                :param iterable; the iterable dict
-                :type iterable: dictionary-itemiterator
-                :returns: the resized dictionary
-                :rtype : dict
-                """
-                return dict(list(islice(iterable, n)))
-
-            return take(settings.PRICE_MONITOR_AMAZON_PRODUCT_SYNCHRONIZE_COUNT, products.iteritems()), True
-
-    def sync_product(self, amazon_product, product):
-        """
-        Synchronizes the given price_monitor.model.Product with the Amazon lookup product.
-        :param amazon_product: the Amazon product
-        :type amazon_product: amazon.api.AmazonProduct
-        :param product: the product to update
-        :type product: price_monitor.models.Product
-        """
-        now = timezone.now()
-
-        product.set_values_from_amazon_product(amazon_product)
-        product.status = 1
-        product.date_last_synced = now
-        product.save()
-
-        # tuple: (price, currency)
-        price = amazon_product.price_and_currency
-
-        if not price[0] is None:
-            # get all subscriptions of product that are subscribed to the current price or a higher one and
-            # whose owners have not been notified about that particular subscription price since before
-            # settings.PRICE_MONITOR_SUBSCRIPTION_RENOTIFICATION_MINUTES.
-            for sub in Subscription.objects.filter(
-                Q(
-                    product=product,
-                    price_limit__gte=price[0],
-                    date_last_notification__lte=(timezone.now() - timedelta(minutes=settings.PRICE_MONITOR_SUBSCRIPTION_RENOTIFICATION_MINUTES))
-                ) | Q(
-                    product=product,
-                    price_limit__gte=price[0],
-                    date_last_notification__isnull=True
-                )
-            ):
-                # TODO: how to handle failed notifications?
-                NotifySubscriberTask().delay(product, price[0], price[1], sub)
-
-            # create the price entry
-            Price.objects.create(
-                value=price[0],
-                currency=price[1],
-                date_seen=now,
-                product=product,
-            )
 
 
 class NotifySubscriberTask(Task):
