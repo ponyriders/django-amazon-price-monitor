de7f12085686c3509ce15c4e55004c3c8b944f98
Martin Mrose <martin@penthranet.de>
2014-05-25 14:45:09 +0200

    Added subscription crud view

--- price_monitor/api/urls.py
+++ price_monitor/api/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url
 
 from price_monitor.api.views.ProductCRUDView import ProductCRUDView
+from price_monitor.api.views.SubscriptCRUDView import SubscriptionCRUDView
 
 
 urlpatterns = patterns(
     '',
     # url(r'^products/$', ProductListView.as_view(), name='userpost-list'),
-    url(r'^product-crud/$', ProductCRUDView.as_view()),
+    url(r'^product-crud/$', ProductCRUDView.as_view(), name='api_product_crud'),
+    url(r'^subscription-crud/$', SubscriptionCRUDView.as_view(), name='api_subscription_crud'),
 )
