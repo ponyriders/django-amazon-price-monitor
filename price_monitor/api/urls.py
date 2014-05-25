40a2d24999c58784a8eb9c2c371777d2001e22b7
Martin Mrose <martin@penthranet.de>
2014-05-25 21:21:55 +0200

    Made trailing slash optional

--- price_monitor/api/urls.py
+++ price_monitor/api/urls.py
@@ -7,6 +7,6 @@ from price_monitor.api.views.SubscriptCRUDView import SubscriptionCRUDView
 urlpatterns = patterns(
     '',
     # url(r'^products/$', ProductListView.as_view(), name='userpost-list'),
-    url(r'^product-crud/$', ProductCRUDView.as_view(), name='api_product_crud'),
-    url(r'^subscription-crud/$', SubscriptionCRUDView.as_view(), name='api_subscription_crud'),
+    url(r'^product-crud/?$', ProductCRUDView.as_view(), name='api_product_crud'),
+    url(r'^subscription-crud/?$', SubscriptionCRUDView.as_view(), name='api_subscription_crud'),
 )
