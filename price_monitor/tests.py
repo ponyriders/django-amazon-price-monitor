b30b1efc4312598684f25b1a584b649c5d615b9a
Alexander Herrmann <darignac@googlemail.com>
2014-05-28 18:35:46 +0200

    added test for the subscription deletion view

--- price_monitor/tests.py
+++ price_monitor/tests.py
@@ -1,13 +1,20 @@
-import unittest
+from price_monitor.models import (
+    EmailNotification,
+    Product,
+    Subscription,
+)
 
-from .models import Product
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django.utils import unittest
 
 from lxml import objectify
 
 from amazon.api import AmazonProduct
 
 
-class ProductTest(unittest.TestCase):
+class ProductTest(TestCase):
 
     # General XML, structure taken from a Blu-Ray, I replaced the ASIN, titles and descriptions, assoc tag, image identifiers, and more
     # ASIN: X100Y200WZ
@@ -572,6 +579,68 @@ class ProductTest(unittest.TestCase):
 
     def __get_sample_amazon_product(self):
         return AmazonProduct(objectify.fromstring(self.test_xml), None, None, None)
+
+
+class SubscriptionTest(TestCase):
+    """
+    Test class for the Subscription model.
+    """
+    urls = 'price_monitor.urls'
+
+    def setUp(self):
+        self.products = [
+            Product.objects.create(asin='A1'),
+            Product.objects.create(asin='A2'),
+            Product.objects.create(asin='A3'),
+        ]
+
+    def tearDown(self):
+        for p in self.products:
+            p.delete()
+
+    def test_delete_subscription(self):
+        """
+        Tests the deletion of an subscription via the delete_subscription_view.
+        """
+        # create a test user that is the subscription owner
+        user_username = 'john'
+        user_password = 'p4ssw0rd'
+        owner = User.objects.create_user(user_username, 'john@example.com', user_password)
+
+        # add his email notification
+        email_notification = EmailNotification.objects.create(
+            owner=owner,
+            email='dummy@example.com',
+        )
+
+        # create a subscription for a product
+        s = Subscription.objects.create(
+            owner=owner,
+            product=self.products[0],
+            price_limit=10.0,
+            email_notification=email_notification
+        )
+
+        # check that it's there
+        self.assertTrue(s)
+        self.assertTrue(len(s.public_id) == 36)
+
+        # log the user in, requires django.contrib.sessions in INSTALLED_APPS
+        self.assertTrue(
+            self.client.login(username=user_username, password=user_password)
+        )
+
+        # get url for deletion and call it
+        response = self.client.get(
+            reverse('delete_subscription_view', kwargs={'public_id': s.public_id}),
+            follow=True
+        )
+
+        # should be successful
+        self.assertEqual(response.status_code, 200)
+
+        # subscription shall be deleted now
+        self.assertRaises(Subscription.DoesNotExist, lambda: Subscription.objects.get(public_id=s.public_id))
 
 
 if __name__ == '__main__':
