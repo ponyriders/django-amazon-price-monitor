342237682c37701b39dadbfab72016c956331955
Martin Mrose <martin@penthranet.de>
2014-06-15 17:00:35 +0200

    Subscription now serializes product nested to minimize initial requests

--- price_monitor/api/serializers/SubscriptionSerializer.py
+++ price_monitor/api/serializers/SubscriptionSerializer.py
@@ -1,16 +1,18 @@
+from price_monitor.api.serializers.ProductSerializer import ProductSerializer
 from price_monitor.models import Subscription
 
 from rest_framework import serializers
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
 
     class Meta:
         model = Subscription
         fields = (
             'public_id',
-            'product',
             'price_limit',
+            'product',
             'date_last_notification',
             'email_notification',
         )
