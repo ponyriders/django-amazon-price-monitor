5b7ccd3123629b9680363adf70655feb2c1b7ac5
Alexander Herrmann <darignac@googlemail.com>
2014-06-17 07:18:18 +0200

    don't synchronize products that are marked as not synchable #21

--- price_monitor/tasks.py
+++ price_monitor/tasks.py
@@ -94,7 +94,9 @@ class ProductSynchronizationMixin(object):
         products = {
             p.asin: p for p in Product.objects.select_related().filter(
                 subscription__isnull=False,
-                date_last_synced__lte=(timezone.now() - timedelta(minutes=settings.PRICE_MONITOR_AMAZON_PRODUCT_REFRESH_THRESHOLD_MINUTES))
+                date_last_synced__lte=(timezone.now() - timedelta(minutes=settings.PRICE_MONITOR_AMAZON_PRODUCT_REFRESH_THRESHOLD_MINUTES)),
+                # issue #21 don't sync products that are  not existent
+                status__in=[0, 1]
             )
         }
 
