1cac819276f928751bb7a12992c88a3c48899858
Martin Mrose <martin@penthranet.de>
2014-06-22 18:31:52 +0200

    Fixed url name

--- price_monitor/api/urls.py
+++ price_monitor/api/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns(
     '',
     url(r'^email-notifications/$', EmailNotificationListView.as_view(), name='api_email_notification_list'),
     url(r'^products/(?P<asin>[0-9a-zA-Z_-]+)/prices/$', PriceListView.as_view(), name='api_product_price_list'),
-    url(r'^products/(?P<asin>[0-9a-zA-Z_-]+)/$', ProductRetrieveView.as_view(), name='api_product_list'),
+    url(r'^products/(?P<asin>[0-9a-zA-Z_-]+)/$', ProductRetrieveView.as_view(), name='api_product_retrieve'),
     url(r'^products/$', ProductListView.as_view(), name='api_product_list'),
     url(r'^subscriptions/(?P<public_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$', SubscriptionRetrieveView.as_view(), name='api_subscription_retrieve'),
     url(r'^subscriptions/$', SubscriptionListView.as_view(), name='api_subscription_list'),
