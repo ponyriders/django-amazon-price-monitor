3f126257622908fd9679e4cd8dd9d37db961f907
Martin Mrose <martin@penthranet.de>
2014-06-30 22:49:37 +0200

    Added pagination

--- price_monitor/static/price_monitor/app/js/controller/product-ctrl.js
+++ price_monitor/static/price_monitor/app/js/controller/product-ctrl.js
@@ -12,6 +12,23 @@ PriceMonitorApp.controller('ProductCtrl', function($scope, Product, Subscription
             angular.forEach($scope.subscriptions, function(subscription) {
                 subscription.fetchedProduct = sortedProducts[subscription.product];
             });
+            
+            $scope.productCount = $scope.subscriptions.length;
+            $scope.currentPage = 1;
+            $scope.maxPageCount = SETTINGS.pagination.maxPageCount;
+            $scope.itemsPerPage = SETTINGS.pagination.itemsPerPage;
+            $scope.paginationBoundaryLinks = SETTINGS.pagination.paginationBoundaryLinks; 
+            $scope.paginationRotate = SETTINGS.pagination.paginationRotate;
+            $scope.pagesTotal = 0;
         });
     });
+});
+
+//We already have a limitTo filter built-in to angular,
+//let's make a startFrom filter
+PriceMonitorApp.filter('startFrom', function() {
+    return function(input, start) {
+        start = parseInt(start); //parse to int
+        return input.slice(start);
+    }
 });
