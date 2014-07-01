a9b8345521c328ee595496a0abbd72d2231568c9
Martin Mrose <martin@penthranet.de>
2014-07-01 21:16:43 +0200

    Updated public id mixin as setting default to uuid4 return did not work on master branch

--- price_monitor/models/mixins/PublicIDMixin.py
+++ price_monitor/models/mixins/PublicIDMixin.py
@@ -1,7 +1,7 @@
-import uuid
-
 from django.db import models
 from django.utils.translation import ugettext as _
+
+from uuid import uuid4
 
 
 class PublicIDMixin(models.Model):
@@ -10,13 +10,26 @@ class PublicIDMixin(models.Model):
     """
     public_id = models.CharField(
         max_length=36,
-        default=str(uuid.uuid4()),
         unique=True,
         editable=False,
         null=False,
         db_index=True,
         verbose_name=_('Public-ID')
     )
+
+    def save(self, *args, **kwargs):
+        """
+        Sets public id on new instances
+        :param args: positional arguments
+        :type args: list
+        :param kwargs: keyword arguments
+        :type kwargs: dict
+        :returns: what parent returns
+        :rtype: see parent
+        """
+        if self.pk is None:
+            self.public_id = str(uuid4())
+        return super(PublicIDMixin, self).save(*args, **kwargs)
 
     class Meta:
         abstract = True
