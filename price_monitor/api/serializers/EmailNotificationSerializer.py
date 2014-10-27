dd5e14fe21b2ac053c86250bdf6c8b29b38996dd
Martin Mrose <martin@penthranet.de>
2014-10-27 21:36:15 +0100

    Removed id from email notfication serializer

--- price_monitor/api/serializers/EmailNotificationSerializer.py
+++ price_monitor/api/serializers/EmailNotificationSerializer.py
@@ -7,11 +7,7 @@ class EmailNotificationSerializer(serializers.ModelSerializer):
     """
     Serializes EmailNotification objects. Just renders public_id as id and the email address
     """
-    id = serializers.CharField(source='public_id')
 
     class Meta:
         model = EmailNotification
-        fields = (
-            'id',
-            'email',
-        )
+        fields = ('email',)
