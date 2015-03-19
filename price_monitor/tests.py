71f4b4087d05d3bc49e396f219aa291a7513229c
Alexander Herrmann <darignac@gmail.com>
2015-03-19 17:54:39 +0100

    started with some test skeletons  #27

--- price_monitor/tests.py
+++ price_monitor/tests.py
@@ -9,6 +9,37 @@ from price_monitor.models import (
     Product,
     Subscription,
 )
+from price_monitor.product_advertising_api.api import ProductAdvertisingAPI
+
+
+class ProductAdvertisingAPITest(TestCase):
+    """
+    Test class for the ProductAdvertisingAPI.
+    """
+
+    def setUp(self):
+        self.api = ProductAdvertisingAPI()
+
+    def tearDown(self):
+        pass
+
+    def test_item_lookup_normal(self):
+        """
+        Test for a normal product.
+        """
+        # TODO implement
+
+    def test_item_lookup_no_price(self):
+        """
+        Test for a product without price.
+        """
+        # TODO implement
+
+    def test_item_lookup_no_audience_rating(self):
+        """
+        Test for a product without audience rating.
+        """
+        # TODO implement
 
 
 class ProductTest(TestCase):
