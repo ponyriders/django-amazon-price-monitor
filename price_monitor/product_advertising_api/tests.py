816681ea2c9851cbef8df457d62cfb01d9d08a10
Alexander Herrmann <darignac@gmail.com>
2015-03-24 16:35:09 +0100

    finished test_item_lookup_normal  #27

--- price_monitor/product_advertising_api/tests.py
+++ price_monitor/product_advertising_api/tests.py
@@ -1,3 +1,5 @@
+import datetime
+
 from bs4 import BeautifulSoup
 
 from django.test import TestCase
@@ -96,7 +98,7 @@ class ProductAdvertisingAPITest(TestCase):
                     <isvalid>True</isvalid>
                     <itemlookuprequest>
                         <idtype>ASIN</idtype>
-                        <itemid>DEMOASIN03</itemid>
+                        <itemid>DEMOASIN02</itemid>
                         <responsegroup>Large</responsegroup>
                         <variationpage>All</variationpage>
                     </itemlookuprequest>
@@ -143,7 +145,7 @@ class ProductAdvertisingAPITest(TestCase):
                     </itemlookuprequest>
                 </request>
                 <item>
-                    <asin>DEMOASIN02</asin>
+                    <asin>DEMOASIN03</asin>
                     <itemattributes>
                         <title>Demo Series - Season 2 [Blu-ray]</title>
                         <binding>Blu-ray</binding>
@@ -186,28 +188,44 @@ class ProductAdvertisingAPITest(TestCase):
     @patch.object(ProductAdvertisingAPI, 'lookup_at_amazon')
     @patch.object(ProductAdvertisingAPI, '__init__')
     @log_capture()
-    def test_item_lookup_normal(self, papi_init, papi_lookup, lc):
+    def test_item_lookup_normal(self, product_api_init, product_api_lookup, lc):
         """
         Test for a normal bluray.
-        :param papi_init: mockup for ProductAdvertisingAPI.__init__
-        :type papi_init: unittest.mock.MagicMock
-        :param papi_lookup: mockup for ProductAdvertisingAPI.lookup_at_amazon
-        :type papi_init: unittest.mock.MagicMock
+        :param product_api_init: mockup for ProductAdvertisingAPI.__init__
+        :type product_api_init: unittest.mock.MagicMock
+        :param product_api_lookup: mockup for ProductAdvertisingAPI.lookup_at_amazon
+        :type product_api_lookup: unittest.mock.MagicMock
         :param lc: log capture instance
         :type lc: testfixtures.logcapture.LogCaptureForDecorator
         """
-        papi_init.return_value = None
-        papi_lookup.return_value = self.__get_product_bs(self.product_sample_ok)
+        product_api_init.return_value = None
+        product_api_lookup.return_value = self.__get_product_bs(self.product_sample_ok)
 
         api = ProductAdvertisingAPI()
         values = api.item_lookup('DEMOASIN03')
+
+        # ensure the mocks were called
+        self.assertTrue(product_api_init.called)
+        self.assertTrue(product_api_lookup.called)
+
         self.assertNotEqual(None, values)
+        self.assertEqual(type(dict()), type(values))
         self.assertEqual(len(values), 14)
 
-        # TODO test more
-
-        self.assertTrue(papi_init.called)
-        self.assertTrue(papi_lookup.called)
+        self.assertEqual('DEMOASIN03', values['asin'])
+        self.assertEqual('Demo Series - Season 2 [Blu-ray]', values['title'])
+        self.assertEqual(None, values['isbn'])
+        self.assertEqual(None, values['eisbn'])
+        self.assertEqual('Blu-ray', values['binding'])
+        self.assertEqual(datetime.datetime(2014, 12, 22), values['date_publication'])
+        self.assertEqual(datetime.datetime(2014, 12, 22), values['date_release'])
+        self.assertEqual('http://ecx.images-amazon.com/images/I/DEMOASIN03IMAGE.jpg', values['large_image_url'])
+        self.assertEqual('http://ecx.images-amazon.com/images/I/DEMOASIN03IMAGE._SL160_.jpg', values['medium_image_url'])
+        self.assertEqual('http://ecx.images-amazon.com/images/I/DEMOASIN03IMAGE._SL75_.jpg', values['small_image_url'])
+        self.assertEqual('http://www.amazon.de/dp/DEMOASIN03/?tag=sample-assoc-tag', values['offer_url'])
+        self.assertEqual(16, values['audience_rating'])
+        self.assertEqual(17.99, values['price'])
+        self.assertEqual('EUR', values['currency'])
 
         # check log output, should be empty
         lc.check()
