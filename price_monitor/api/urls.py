5c6d5765f99b1a7c31bc78c993f62b82628d7d10
Martin Mrose <github@penthranet.de>
2015-04-21 21:01:49 +0200

    Product in class name somehow went missing

--- price_monitor/api/urls.py
+++ price_monitor/api/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from .views.EmailNotificationListView import EmailNotificationListView
 from .views.PriceListView import PriceListView
 from .views.ProductListCreateView import ProductListCreateView
-from .views.RetrieveUpdateDestroyAPIView import RetrieveUpdateDestroyAPIView
+from .views.ProductRetrieveUpdateDestroyAPIView import ProductRetrieveUpdateDestroyAPIView
 from .views.SubscriptionRetrieveView import SubscriptionRetrieveView
 from .views.SubscriptionListView import SubscriptionListView
 
@@ -12,7 +12,7 @@ urlpatterns = patterns(
     '',
     url(r'^email-notifications/$', EmailNotificationListView.as_view(), name='api_email_notification_list'),
     url(r'^products/(?P<asin>(:asin|[0-9a-zA-Z_-]+))/prices/$', PriceListView.as_view(), name='api_product_price_list'),
-    url(r'^products/(?P<asin>(:asin|[0-9a-zA-Z_-]+))/$', RetrieveUpdateDestroyAPIView.as_view(), name='api_product_retrieve'),
+    url(r'^products/(?P<asin>(:asin|[0-9a-zA-Z_-]+))/$', ProductRetrieveUpdateDestroyAPIView.as_view(), name='api_product_retrieve'),
     url(r'^products/$', ProductListCreateView.as_view(), name='api_product_list'),
     url(r'^subscriptions/(?P<public_id>(:public_id|[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}))/$', SubscriptionRetrieveView.as_view(), name='api_subscription_retrieve'),
     url(r'^subscriptions/$', SubscriptionListView.as_view(), name='api_subscription_list'),
