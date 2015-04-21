91daf2d9341ef09d86af0a1826eccec25914670b
Martin Mrose <github@penthranet.de>
2015-04-21 20:55:37 +0200

    Only show detail page when product is present, else redirect to list

--- price_monitor/static/price_monitor/app/js/controller/product-detail-ctrl.js
+++ price_monitor/static/price_monitor/app/js/controller/product-detail-ctrl.js
@@ -1,21 +1,28 @@
 PriceMonitorApp.controller('ProductDetailCtrl', function ($scope, $routeParams, $location, $modal, Product) {
-    $scope.product = Product.get({asin: $routeParams.asin});
+    $scope.product = Product.get(
+        {asin: $routeParams.asin},
+        function () {
+            $scope.open = function () {
+                var modalInstance = $modal.open({
+                    templateUrl: SETTINGS.uris.static + '/price_monitor/app/partials/product-delete.html',
+                    controller: 'ProductDeleteCtrl',
+                    size: 'sm',
+                    resolve: {
+                        product: function () {
+                            return $scope.product;
+                        }
+                    }
+                });
 
-    $scope.open = function () {
-
-        var modalInstance = $modal.open({
-            templateUrl: SETTINGS.uris.static + '/price_monitor/app/partials/product-delete.html',
-            controller: 'ProductDeleteCtrl',
-            size: 'sm',
-            resolve: {
-                product: function () {
-                    return $scope.product;
-                }
-            }
-        });
-
-        modalInstance.result.then(function () {
+                modalInstance.result.then(function () {
+                    $location.path('#products');
+                });
+            };
+        },
+        function () {
             $location.path('#products');
-        });
-    };
+        }
+    );
+
+
 });
