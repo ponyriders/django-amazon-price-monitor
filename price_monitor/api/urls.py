bbdda45d716678863ff34bbe6b62b1989a32d2cc
Martin Mrose <github@penthranet.de>
2015-05-05 21:24:46 +0200

    Removed :asin as this does not work vial url template tag

--- price_monitor/api/urls.py
+++ price_monitor/api/urls.py
@@ -11,8 +11,8 @@ from .views.SubscriptionListView import SubscriptionListView
 urlpatterns = patterns(
     '',
     url(r'^email-notifications/$', EmailNotificationListView.as_view(), name='api_email_notification_list'),
-    url(r'^products/(?P<asin>(:asin|[0-9a-zA-Z_-]+))/prices/$', PriceListView.as_view(), name='api_product_price_list'),
-    url(r'^products/(?P<asin>(:asin|[0-9a-zA-Z_-]+))/$', ProductRetrieveUpdateDestroyAPIView.as_view(), name='api_product_retrieve'),
+    url(r'^products/(?P<asin>[0-9a-zA-Z_-]+)/prices/$', PriceListView.as_view(), name='api_product_price_list'),
+    url(r'^products/(?P<asin>[0-9a-zA-Z_-]+)/$', ProductRetrieveUpdateDestroyAPIView.as_view(), name='api_product_retrieve'),
     url(r'^products/$', ProductListCreateView.as_view(), name='api_product_list'),
     url(r'^subscriptions/(?P<public_id>(:public_id|[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}))/$', SubscriptionRetrieveView.as_view(), name='api_subscription_retrieve'),
     url(r'^subscriptions/$', SubscriptionListView.as_view(), name='api_subscription_list'),
