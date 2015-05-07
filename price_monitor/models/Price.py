d5ef004adcf30264d9761ba51f692892d1d24260
Martin Mrose <github@penthranet.de>
2015-05-07 22:13:41 +0200

    Removed public id from prices as it is not needed

--- price_monitor/models/Price.py
+++ price_monitor/models/Price.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils.translation import ugettext as _, ugettext_lazy
 
 
-class Price(PublicIDMixin, models.Model):
+class Price(models.Model):
     """
     Representing fetched price for a product
     """
