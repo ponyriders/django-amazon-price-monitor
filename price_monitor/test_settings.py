f81e4e38cc7dfce102f2541d18349a6104a0035f
Alexander Herrmann <darignac@gmail.com>
2015-06-05 15:17:07 +0200

    removing django.contrib.messages.middleware.MessageMiddleware as not needed #32

--- price_monitor/test_settings.py
+++ price_monitor/test_settings.py
@@ -15,7 +15,6 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
