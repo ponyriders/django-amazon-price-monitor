bdd8ea959a99f202976e63f910cc2c8920cd7ee6
Alexander Herrmann <darignac@gmail.com>
2015-06-05 15:13:15 +0200

    satisfying the system check framework upon test runs #32

--- price_monitor/test_settings.py
+++ price_monitor/test_settings.py
@@ -10,6 +10,15 @@ DATABASES = {
     }
 }
 
+MIDDLEWARE_CLASSES = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
