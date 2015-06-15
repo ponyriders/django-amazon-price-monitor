0f2882e6d50c8f26b14b1aeb93107aff698764cb
Alexander Herrmann <darignac@gmail.com>
2015-06-15 13:59:23 +0200

    be able to resync product in admin site

--- price_monitor/admin.py
+++ price_monitor/admin.py
@@ -18,11 +18,22 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('asin', 'title', 'status', 'date_updated', 'date_last_synced', )
     search_fields = ('asin', )
 
-    actions = ['reset_to_created', ]
+    actions = ['reset_to_created', 'resynchronize', ]
 
     def reset_to_created(self, request, queryset):
         queryset.update(status=0)
     reset_to_created.short_description = ugettext_lazy('Reset to status "Created".')
+
+    def resynchronize(self, request, queryset):
+        """
+        Synchronizes the sent products with the product advertising api.
+        :param request: sent request
+        :param queryset: queryset containing the products
+        """
+        from price_monitor.product_advertising_api.tasks import SynchronizeSingleProductTask
+        for product in queryset:
+            SynchronizeSingleProductTask.delay([product.asin])
+    resynchronize.short_description = ugettext_lazy('Resynchronize with API')
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
