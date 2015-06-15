5203e998bbea906cd88c1df7a6e55c89a0ec72a1
Alexander Herrmann <darignac@gmail.com>
2015-06-15 14:00:48 +0200

    added status to list filter

--- price_monitor/admin.py
+++ price_monitor/admin.py
@@ -16,6 +16,7 @@ class PriceAdmin(admin.ModelAdmin):
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('asin', 'title', 'status', 'date_updated', 'date_last_synced', )
+    list_filter = ('status', )
     search_fields = ('asin', )
 
     actions = ['reset_to_created', 'resynchronize', ]
