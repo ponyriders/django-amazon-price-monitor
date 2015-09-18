fc0f6b7b7eb9ee852ba73d4b32e0302505ab9311
Alexander Herrmann <darignac@gmail.com>
2015-09-18 09:51:30 +0200

    fixed pep8 #2

--- price_monitor/api/urls.py
+++ price_monitor/api/urls.py
@@ -14,6 +14,10 @@ urlpatterns = patterns(
     url(r'^products/(?P<asin>[0-9a-zA-Z_-]+)/prices/$', PriceListView.as_view(), name='api_product_price_list'),
     url(r'^products/(?P<asin>[0-9a-zA-Z_-]+)/$', ProductCreateRetrieveUpdateDestroyAPIView.as_view(), name='api_product_retrieve'),
     url(r'^products/$', ProductListView.as_view(), name='api_product_list'),
-    url(r'^subscriptions/(?P<public_id>(:public_id|[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}))/$', SubscriptionRetrieveView.as_view(), name='api_subscription_retrieve'),
+    url(
+        r'^subscriptions/(?P<public_id>(:public_id|[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}))/$',
+        SubscriptionRetrieveView.as_view(),
+        name='api_subscription_retrieve'
+    ),
     url(r'^subscriptions/$', SubscriptionListView.as_view(), name='api_subscription_list'),
 )
