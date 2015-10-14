3b0e642a069ca6940cfdd6da67648374d17bdc46
Alexander Herrmann <darignac@gmail.com>
2015-10-14 21:01:51 +0200

    angular is now the default frontend closes #70

--- price_monitor/urls.py
+++ price_monitor/urls.py
@@ -4,9 +4,9 @@ from price_monitor.views import AngularIndexView, ProductListAndCreateView
 
 urlpatterns = patterns(
     'price_monitor.views',
+    url(r'^$', AngularIndexView.as_view(), name='angular_view'),
     url(r'^page/(?P<page>\d+)/$', ProductListAndCreateView.as_view(), name='monitor_view'),
-    url(r'^$', ProductListAndCreateView.as_view(), name='monitor_view'),
     url(r'^subscription/delete/(?P<public_id>[a-z0-9\-]{36})/$', 'delete_subscription_view', name='delete_subscription_view'),
     url(r'^api/', include('price_monitor.api.urls')),
-    url(r'^angular/$', AngularIndexView.as_view(), name='angular_view'),
+    url(r'^oldfe/$', ProductListAndCreateView.as_view(), name='monitor_view'),
 )
