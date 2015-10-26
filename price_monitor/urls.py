64cb71340bd5b1e0d6a526e172c609b663c04e26
Alexander Herrmann <darignac@gmail.com>
2015-10-26 20:48:04 +0100

    removed everything relevant for the old frontend and currently unused in the "new" code #73

--- price_monitor/urls.py
+++ price_monitor/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls import patterns, url, include
 
-from price_monitor.views import AngularIndexView, ProductListAndCreateView
+from price_monitor.views import AngularIndexView
 
 urlpatterns = patterns(
     'price_monitor.views',
     url(r'^$', AngularIndexView.as_view(), name='angular_view'),
-    url(r'^page/(?P<page>\d+)/$', ProductListAndCreateView.as_view(), name='monitor_view'),
-    url(r'^subscription/delete/(?P<public_id>[a-z0-9\-]{36})/$', 'delete_subscription_view', name='delete_subscription_view'),
     url(r'^api/', include('price_monitor.api.urls')),
-    url(r'^oldfe/$', ProductListAndCreateView.as_view(), name='monitor_view'),
 )
