02b2199887844eb17c39b7cadaaca31ed2b954ba
Alexander Herrmann <darignac@gmail.com>
2015-12-14 17:37:08 +0100

    releasing 0.5.0

--- price_monitor/__init__.py
+++ price_monitor/__init__.py
@@ -3,7 +3,7 @@ django-amazon-price-monitor monitors prices of Amazon products.
 """
 __version_info__ = {
     'major': 0,
-    'minor': 4,
+    'minor': 5,
     'micro': 0,
     'releaselevel': 'final',
     'serial': 0,
