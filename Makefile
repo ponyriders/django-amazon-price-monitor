30e7c54fa1c9e747645692002d779af507e1257a
Alexander Herrmann <darignac@gmail.com>
2016-03-19 11:29:38 +0100

    added basic docker setup - not working yet

---
+++ Makefile
@@ -0,0 +1,22 @@
+help:
+	@echo "docker-build-web:  - builds the web docker image"
+	@echo "docker-up:         - uses docker-compose to bring the containers up"
+	@echo "docker-stop:       - uses docker-compose to stop the containers"
+	@echo "docker-ps:         - runs docker-compose ps"
+
+docker-build-web:
+	cp setup.py docker/web/django-amazon-price-monitor/setup.py
+	sed -i 's/readme = .*/readme = ""/g' docker/web/django-amazon-price-monitor/setup.py
+	sed -i 's/history = .*/history = ""/g' docker/web/django-amazon-price-monitor/setup.py
+	docker build -t pricemonitor/web docker/web/
+
+docker-up:
+	cd docker && docker-compose up -d
+
+docker-stop:
+	cd docker && docker-compose stop
+
+docker-ps:
+	cd docker && docker-compose ps
+
+docker-reload: docker-build-web docker-up
---
+++ docker/web/django-amazon-price-monitor/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = ""
+history = ""
+
+setup(
+    name='django-amazon-price-monitor',
+    version=__import__('price_monitor').get_version().replace(' ', '-'),
+    description='Monitors prices of Amazon products via Product Advertising API',
+    long_description=readme + '\n\n' + history,
+    author='Alexander Herrmann, Martin Mrose',
+    author_email='django-amazon-price-monitor@googlegroups.com',
+    url='https://github.com/ponyriders/django-amazon-price-monitor',
+    packages=[
+        'price_monitor'
+    ],
+    include_package_data=True,
+    install_requires=[
+        # main dependencies
+        'Django>=1.8,<1.10',
+        # for product advertising api
+        'beautifulsoup4',
+        'bottlenose>=0.6.2',
+        'celery>=3',
+        'python-dateutil>=2.4.2',
+        # for pm api
+        'djangorestframework>=3.3',
+        # for graphs
+        'pygal>=2.0.7',
+        'lxml',
+        # pygal png output
+        'CairoSVG',
+        'tinycss',
+        'cssselect',
+    ],
+    license='MIT',
+    zip_safe=False,
+)
