a30d03fc1dd8e82fd7d3b33d6c9e0d89e538dedb
Alexander Herrmann <darignac@gmail.com>
2016-03-20 11:52:35 +0100

    some docstring

--- docker/web/django-amazon-price-monitor/price_monitor/__init__.py
+++ docker/web/django-amazon-price-monitor/price_monitor/__init__.py
@@ -1,2 +1,6 @@
 def get_version():
+    """
+    Returns DEV as version. Just a placeholder while building the web docker image, will be overwritten by mounted docker volume.
+    :return: the version identifier
+    """
     return 'DEV'
