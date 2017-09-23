2c3cc04aa4a50f03a12532055b80a32614d37e68
Alexander Herrmann <darignac@gmail.com>
2017-09-23 15:47:41 +0200

    chnaged to new MIDDLEWARE setting #113

--- docker/web/project/glue/settings.py
+++ docker/web/project/glue/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'price_monitor.product_advertising_api',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -49,7 +50,6 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'glue.urls'
